a5d577f7e0e977cadeac8467be60eb9be1bbdf31
Daniel <daniel.jungen@t-online.de>
2023-09-29 20:37:14 +0200

    4

--- Shares_AVG.R
+++ Shares_AVG.R
@@ -1,38 +1,50 @@
-library(tidyquant)
-
-options("getSymbols.warning4.0"=FALSE)
-options("getSymbols.yahoo.warning"=FALSE)
-# Downloading Apple price using quantmod
-
-getSymbols("ADS.DE", from = '2017-01-01',
-           to = "2023-09-20",warnings = FALSE,
-           auto.assign = TRUE)
+library(tidyverse)
 
 
-ADS<-na.omit(data.frame(ADS.DE[,4])) 
 
+setwd("C:\\Users\\danie\\Documents\\shares_avg\\downloads\\")
 
-<<<<<<< HEAD
-for(i in 25:dim(ADS)[1])
-{ADS[i,2]<-mean(ADS[(i-24):(i),1])}
+pfad="C:\\Users\\danie\\Documents\\shares_avg\\downloads\\"
 
-for(i in 50:dim(ADS)[1])
-{ADS[i,3]<-mean(ADS[(i-49):(i),1])}
+filenames <- list.files(path=, pattern=".csv", all.files=FALSE, full.names=FALSE)
 
-for(i in 200:dim(ADS)[1])
-{ADS[i,4]<-mean(ADS[(i-199):(i),1])}
+i<-1
+for(i in 1:1){
+        
+        infile<-read.csv(filenames[i])
+        shares<-data.frame(cbind(infile$Date, infile$Close))
+        names(shares)<-c("Date", "Close")
+        
+        shares$Close<-as.numeric(shares$Close)
+        
+        shares<-na.omit(shares)
+        
+        for(j in 25:dim(shares)[1])
+        {shares[j,3]<-mean(shares[(j-24):(j),2])}
+        
+        for(j in 50:dim(shares)[1])
+        {shares[j,4]<-mean(shares[(j-49):(j),2])}
+        
+        for(j in 200:dim(shares)[1])
+        {shares[j,5]<-mean(shares[(j-199):(j),2])
+        shares[j,6]<-j-199}
+        
+        names(shares)<-c("Date", "Kurs", "GD25", "GD50", "GD200", "Laufindex")
+        shares<-na.omit(shares)
+        
+        #shares<-shares[4000:5865,]
+        #shares$Laufindex<-c(1:(dim(shares)[1]))
+        row.names(shares)<-c(1:(dim(shares)[1]))
 
-names(ADS)<-c("Kurs", "GD25", "GD50", "GD200")
-
-ADS<-na.omit(ADS)
-
-n<-dim(ADS)[1]
-ADS$Empfehlung30_200<-""
-ADS$Empfehlung30<-""
-ADS$Empfehlung200<-""
-
-for(i in 1:n){
-        if(ADS$Kurs[i]>ADS$GD30[i] && ADS$Kurs[i]>ADS$GD200[i]){ADS$Empfehlung30_200[i]<-"Kauf"}
-        if(ADS$Kurs[i]>ADS$GD30[i]){ADS$Empfehlung30[i]<-"Kauf"}
-        if(ADS$Kurs[i]>ADS$GD200[i]){ADS$Empfehlung200[i]<-"Kauf"}
+        
+        p= ggplot(data = shares, aes(x = shares$Laufindex)) +
+                geom_line(aes(y = shares$Kurs, colour = "Kurs")) +
+                geom_line(aes(y = shares$GD25, colour = "GD25")) +
+                geom_line(aes(y = shares$GD50, colour = "GD50")) +
+                geom_line(aes(y = shares$GD200, colour = "GD200"))
+        
+        print(p)
 }
+
+
+
